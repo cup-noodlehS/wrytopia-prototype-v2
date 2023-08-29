6d0c8eccc66ee3191eb4bbe6dc406f7c71322902
cup-noodlehS <93570629+cup-noodlehS@users.noreply.github.com>
2023-08-29 22:56:57 +0800

    success

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   name: "cover animation",
 });
+var animationPen = bodymovin.loadAnimation({
+  container: document.getElementById("animation2 container2"),
+  path: "./images/animations/pen.json",
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  name: "pen animation",
+});
