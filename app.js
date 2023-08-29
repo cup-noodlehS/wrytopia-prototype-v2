a2f6f7121c56dfd500c07702fb7b57b1e524a542
cup-noodlehS <93570629+cup-noodlehS@users.noreply.github.com>
2023-08-29 13:56:31 +0800

    success

---
+++ app.js
@@ -0,0 +1,15 @@
+// const slideDown = document.querySelector(".slide-down1");
+
+// // Create an Intersection Observer instance
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       slideDown.style.animation = "slideDown 1.5s";
+//     } else {
+//       slideDown.style.animation = "none";
+//     }
+//   });
+// });
+
+// // Start observing the fade container
+// observer.observe(slideDown);
