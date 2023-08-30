3df0550acfd1c4677344f14a8062624d202f401e
cup-noodlehS <93570629+cup-noodlehS@users.noreply.github.com>
2023-08-30 10:26:34 +0800

    success

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ var animationPen = bodymovin.loadAnimation({
   container: document.getElementById("animation2 container2"),
   path: "./images/animations/pen.json",
   render: "svg",
-  loop: true,
+  loop: false,
   autoplay: true,
   name: "pen animation",
+});
+var animationWhatIs = bodymovin.loadAnimation({
+  container: document.getElementById("animation3 container3"),
+  path: "./images/animations/WhatIs.lottie.json",
+  render: "svg",
+  loop: false,
+  autoplay: true,
+  name: "what animation",
 });
