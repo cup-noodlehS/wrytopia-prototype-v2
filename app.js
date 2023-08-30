fbf5e3e7df8c227eb4a8a48de6e7b168f7a5ae6d
cup-noodlehS <93570629+cup-noodlehS@users.noreply.github.com>
2023-08-30 13:20:49 +0800

    success

--- app.js
+++ app.js
@@ -23,3 +23,39 @@ var animationWhatIs = bodymovin.loadAnimation({
   autoplay: true,
   name: "what animation",
 });
+
+//for services changing
+const creationBtn = document.querySelector("#creation-btn");
+const revisionBtn = document.querySelector("#revision-btn");
+const creationCard = document.querySelector(".outline-creation");
+const revisionCard = document.querySelector(".outline-revision");
+
+creationBtn.addEventListener("click", () => {
+  creationBtn.classList.add("service-active");
+  revisionBtn.classList.remove("service-active");
+  creationCard.style.display = "flex";
+  revisionCard.style.display = "none";
+  // creationCard.classList.add("fade1");
+  // revisionCard.classList.add("fade1");
+
+  creationCard.classList.remove("slide-left1");
+  revisionCard.classList.remove("slide-left1");
+
+  creationCard.classList.add("slide-right1");
+  revisionCard.classList.add("slide-right1");
+});
+
+revisionBtn.addEventListener("click", () => {
+  creationBtn.classList.remove("service-active");
+  revisionBtn.classList.add("service-active");
+  creationCard.style.display = "none";
+  revisionCard.style.display = "flex";
+  // creationCard.classList.add("fade1");
+  // revisionCard.classList.add("fade1");
+
+  creationCard.classList.remove("slide-right1");
+  revisionCard.classList.remove("slide-right1");
+
+  creationCard.classList.add("slide-left1");
+  revisionCard.classList.add("slide-left1");
+});
